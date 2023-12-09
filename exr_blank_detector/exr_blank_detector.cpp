3f0822db8ea275ed32d706e17f7a2feda190ba31
Antonio Drusin <antonio.drusin@gmail.com>
2023-12-09 12:04:34 -0500

    working example:

--- exr_blank_detector/exr_blank_detector.cpp
+++ exr_blank_detector/exr_blank_detector.cpp
@@ -1,29 +1,141 @@
+#define IMATH_DLL
+
 #include <iostream>
 #include <filesystem>
 #include <fstream>
+#include <future>
 #include <string>
 #include "arguments.h"
+#include "ImfRgbaFile.h"
+#include "ImfArray.h"
 
 namespace fs = std::filesystem;
+std::mutex cout_mutex;
+void safePrint(const std::string& message) {
+    std::lock_guard<std::mutex> guard(cout_mutex);
+    std::cout << message << std::endl;
+}
 
-void display_files_recursively(const std::string& folderPath) {
-  
-    if (!fs::exists(folderPath) || !fs::is_directory(folderPath)) {
+void make_file(const std::string& folderPath)
+{
+    try
+    {
+        constexpr int width = 10;
+        constexpr int height = 10;
+
+        Imf::Array2D<Imf::Rgba> pixels(width, height);
+        for (int y = 0; y < height; y++)
+            for (int x = 0; x < width; x++)
+                pixels[y][x] = Imf::Rgba(0, x / (width - 1.0f), y / (height - 1.0f));
+
+        const auto file_name = folderPath + "\\hello.exr";
+        Imf::RgbaOutputFile file(file_name.c_str(), width, height, Imf::WRITE_RGBA);
+        file.setFrameBuffer(&pixels[0][0], 1, width);
+        file.writePixels(height);
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Unable to read image file hello.exr:" << e.what() << std::endl;
+    }
+}
+
+bool are_all_pixels_black(const std::string& filename)
+{
+    try
+    {
+        Imf::RgbaInputFile file(filename.c_str());
+        const Imath::Box2i dw = file.dataWindow();
+
+        const int width = dw.max.x - dw.min.x + 1;
+        const int height = dw.max.y - dw.min.y + 1;
+
+        std::vector<Imf::Rgba> pixels(height * width);
+        file.setFrameBuffer(pixels.data() - dw.min.x - dw.min.y * width, 1, width);
+        file.readPixels(dw.min.y, dw.max.y);
+
+        float zero = 0.0;
+
+        for (const auto& pixel : pixels)
+        {
+            if (pixel.r != zero || pixel.g != zero || pixel.b != zero)
+            {
+                return false; // Found a non-black pixel
+            }
+        }
+
+        return true; // All pixels are black
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+        return false;
+    }
+}
+
+void wait_for_available_slot(const std::vector<std::future<void>>& futures, size_t max_concurrent_tasks)
+{
+    while (futures.size() >= max_concurrent_tasks)
+    {
+        for (auto& future : futures)
+        {
+            if (future.wait_for(std::chrono::milliseconds(100)) == std::future_status::ready)
+            {
+                return; // One future completed, slot available
+            }
+        }
+    }
+}
+
+void analyze_files_recursively(const std::string& folderPath)
+{
+    if (!fs::exists(folderPath) || !fs::is_directory(folderPath))
+    {
         std::cerr << "Path is not a valid directory: " << folderPath << std::endl;
         return;
     }
 
-    try {
-        for (const auto& entry : fs::recursive_directory_iterator(folderPath)) {
-            if (fs::is_regular_file(entry.status())) {
-                std::cout << entry.path() << std::endl;
+    std::vector<std::future<void>> futures;
+    const size_t max_concurrent_tasks = std::thread::hardware_concurrency();
+
+    try
+    {
+        for (const auto& entry : fs::recursive_directory_iterator(folderPath))
+        {
+            //safePrint(entry.path().string());
+           
+            if (fs::is_regular_file(entry.status()))
+            {
+                wait_for_available_slot(futures, max_concurrent_tasks);
+
+                auto future = std::async(std::launch::async, [entry]()
+                {
+                    const auto black = are_all_pixels_black(entry.path().string());
+                    safePrint("result " + entry.path().string() + ": " + (black ? "black" : "not_black"));
+                });
+                
+                futures.push_back(std::move(future));
+
+                // Remove completed futures
+                futures.erase(
+                    std::ranges::remove_if(futures, 
+                                           [](const std::future<void>& f) { return f.wait_for(std::chrono::milliseconds(0)) == std::future_status::ready; }).begin(),
+                    futures.end());
             }
         }
-    } catch (const fs::filesystem_error& e) {
+
+        // Wait for all remaining tasks to complete
+        for (auto& future : futures) {
+            future.wait();
+        }
+    }
+    catch (const fs::filesystem_error& e)
+    {
         std::cerr << "Filesystem error: " << e.what() << std::endl;
-    } catch (const std::exception& e) {
+    } catch (const std::exception& e)
+    {
         std::cerr << "Standard exception: " << e.what() << std::endl;
-    } catch (...) {
+    } catch (...)
+    {
         std::cerr << "Unknown error occurred." << std::endl;
     }
 }
@@ -41,12 +153,13 @@ int main(const int argc, char* argv[])
         std::cerr << R"(exr_blank_detector -f "C:\My Folder")" << std::endl;
         return -1;
     }
-    
-    
+
+
     for (const auto& folder : folders)
     {
         std::cerr << "Processing folder: " << folder << std::endl;
-        display_files_recursively(folder);
+        analyze_files_recursively(folder);
     }
-    
+
+    return 0;
 }
